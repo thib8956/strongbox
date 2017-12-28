117f79b026aba6bd41db138a5603c0bc17b26320
Thibaud <thib8956@users.noreply.github.com>
2017-12-28 15:17:29 +0100

    Add search in keystore functionnality (#4)
    * Search privatekey in the keystore
    
    * Display error message for invalid public key
    
    An error message is now displayed if the provided public key is in an
    invalid format, or was not found in the keystore.
    
    * Add method to strip PEM headers from user input
    
    The PEM headers such as "-----BEGIN PRIVATE KEY-----" are now stripped
    from the user input using a regex.

--- strongbox-server/src/main/java/server/StrongboxHttpsServer.java
+++ strongbox-server/src/main/java/server/StrongboxHttpsServer.java
@@ -10,10 +10,8 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.util.Base64;
+import java.net.URLDecoder;
+import java.security.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -22,11 +20,14 @@ import java.util.logging.Logger;
 public class StrongboxHttpsServer {
 
     private static final String MAIN_CONTEXT = "/";
-    private static final String KEYSTORE_PATH = "src/main/resources/cert.jks";
+    private static final String CERT_KEYSTORE_PATH = "src/main/resources/cert.jks";
     private static final String SUN_X_509 = "SunX509";
     private static final String KEYSTORE_PWD = "password";
+    private static final String PKSERVER = "/pkserver";
+    private static final String ENCODING = "UTF-8";
 
-    private final Logger logger = Logger.getLogger(StrongboxHttpsServer.class.getName());
+    private final static Logger logger = Logger.getLogger(StrongboxHttpsServer.class.getName());
+    public static final String ADD = "/add";
     private HttpsServer httpsServer;
 
     public StrongboxHttpsServer() {
@@ -36,10 +37,11 @@ public class StrongboxHttpsServer {
 
             SSLContext sslContext = initSSLContext();
             httpsServer.setHttpsConfigurator(new StrongboxHttpsConfigurator(sslContext));
-            httpsServer.createContext("/pkserver", new StrongBoxHttpHandler());
+            httpsServer.createContext(PKSERVER, new StrongBoxHttpHandler(PKSERVER));
+            httpsServer.createContext(ADD, new StrongBoxHttpHandler(ADD));
             httpsServer.createContext(MAIN_CONTEXT, new StaticFileHandler("../client"));
-            httpsServer.setExecutor(null);
 
+            httpsServer.setExecutor(null);
         } catch (GeneralSecurityException | IOException e) {
             logger.log(Level.SEVERE, null, e);
         }
@@ -47,7 +49,7 @@ public class StrongboxHttpsServer {
 
     private SSLContext initSSLContext() throws GeneralSecurityException, IOException {
         final SSLContext sslContext = SSLContext.getInstance("TLS");
-        final KeyStoreManager manager = new KeyStoreManager(KEYSTORE_PATH, KEYSTORE_PWD);
+        final KeyStoreManager manager = new KeyStoreManager(CERT_KEYSTORE_PATH, KEYSTORE_PWD);
         KeyStore keystore = manager.getKeyStore();
         final KeyManagerFactory kmf = KeyManagerFactory.getInstance(SUN_X_509);
         kmf.init(keystore, KEYSTORE_PWD.toCharArray());
@@ -69,35 +71,52 @@ public class StrongboxHttpsServer {
 
     private class StrongBoxHttpHandler implements HttpHandler {
 
+        static final String KEYSTORE_PATH = "src/main/resources/keystore.jks";
         Map<String, String> parameters;
+        private String context;
 
-        StrongBoxHttpHandler() {
+
+        StrongBoxHttpHandler(String context) {
             super();
+            this.context = context;
             parameters = new HashMap<>();
         }
 
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody(), "utf-8"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody(), ENCODING));
             String query = reader.readLine();
             parseQuery(query);
 
+            if (context.equals(PKSERVER)) {
+                handlePkserver(httpExchange);
+            } else if (context.equals(ADD)) {
+                handleAdd(httpExchange);
+            }
+        }
+
+        private void handlePkserver(HttpExchange httpExchange) throws IOException {
             StringBuilder response = new StringBuilder();
 
-            // TODO: Check passwd in POST request to access the keystore.
+            String providedB64Key = stripHeaders(parameters.get("publickey")).replaceAll("\\s", "");
+            String password = parameters.get("password");
             try {
-                KeyStoreManager manager = new KeyStoreManager("src/main/resources/keystore.jks", "password");
-                for (Certificate cert : manager.geyCertificates()) {
-                    response.append(cert.getPublicKey().getAlgorithm()).append("\n");
-                    response.append(cert.getPublicKey().getFormat()).append("\n");
-                    response.append(Base64.getEncoder().encodeToString(cert.getEncoded())).append("\n");
-                }
-            } catch (GeneralSecurityException e) {
-                e.printStackTrace();
-            }
+                KeyStoreManager manager = new KeyStoreManager(KEYSTORE_PATH, password);
+                final PublicKey publicKey = KeyStoreManager.getPublicKey(providedB64Key);
 
-            for (Map.Entry<String, String> param : parameters.entrySet()) {
-                response.append(param.getKey()).append(" ").append(param.getValue()).append("\n");
+                final PrivateKey privateKey = manager.getPrivateKey(publicKey, "");
+                if (privateKey == null) {
+                    throw new InvalidKeyException();
+                }
+
+                response.append("Algorithm : ").append(privateKey.getAlgorithm()).append("\n");
+                response.append("Format : ").append(privateKey.getFormat()).append("\n");
+                response.append(KeyStoreManager.privateKeyToString(privateKey));
+            } catch (InvalidKeyException e) {
+                logger.log(Level.WARNING, "Invalid public key", e);
+                response.append("The provided public key is invalid or wasn't found in the keystore.");
+            } catch (GeneralSecurityException e) {
+                logger.log(Level.SEVERE, null, e);
             }
 
             httpExchange.sendResponseHeaders(200, response.length());
@@ -106,15 +125,27 @@ public class StrongboxHttpsServer {
             }
         }
 
-        private void parseQuery(String query) {
+        private void handleAdd(HttpExchange httpExchange) throws IOException {
+            String providedB64Cert = stripHeaders(parameters.get("cert")).replaceAll("\\s", "");
+            String providedB64Key = stripHeaders(parameters.get("privatekey")).replaceAll("\\s", "");
+        }
+
+        private void parseQuery(String query) throws UnsupportedEncodingException {
             if (query == null) {
                 return;
             }
+
             final String[] pairs = query.split("[&]");
             for (String pair : pairs) {
                 final String[] param = pair.split("[=]");
-                parameters.put(param[0], param[1]);
+                String decodedValue =  URLDecoder.decode(param[1], ENCODING);
+
+                parameters.put(param[0], decodedValue);
             }
+        }
+
+        private String stripHeaders(String pem) {
+            return pem.replaceAll("-----(BEGIN|END) ((PUBLIC|PRIVATE) KEY|CERTIFICATE)-----", "");
         }
     }
 
